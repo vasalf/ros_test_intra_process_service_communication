647274029569cf188fed3189c920a91b7156444d
Vasily Alferov <ya-ikmik2012@yandex.ru>
2017-05-10 18:34:48 +0300

    Rename project && add license

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 ## Compiling
 
-Supposing you use [this guide](http://vasalf.net/ros-build.html) to build ROS from git sources.
+Supposing you use [this guide](http://vasalf.net/au/ros-build.html) to build ROS from git sources.
 
 Clone this repo to your workspace source directory, then run from the root of your workspace:
 
 ```
 $ . ./install_isolated/setup.bash
-$ catkin_make_isolated --pkg ros_test_self_service --install
+$ catkin_make_isolated --pkg test_intra_process_service_communication --install
 ```
 
 ## Running
 
 ```
-$ rosrun test_self_service test_self_service
+$ rosrun test_intra_proces_service_communication single_thread
 ```
 
 It should produce some messages every 0.5 seconds.
@@ -20,7 +20,7 @@ It should produce some messages every 0.5 seconds.
 You can check whether the service is not responding with the following command:
 
 ```
-$ rosrun test_self_service call_from_outside
+$ rosrun test_intra_proces_service_communication call_from_outside
 ```
 
 If everything is OK, it should terminate successfully.
--- src/single_thread.cpp
+++ src/single_thread.cpp
@@ -23,17 +23,39 @@ SOFTWARE.
 #include <ros/ros.h>
 #include <std_srvs/Empty.h>
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "tss_outside");
-    ros::NodeHandle nh;
+bool accept_request(std_srvs::Empty::Request &req,
+                    std_srvs::Empty::Response & res) {
+    ROS_INFO("Accepted!");
+    return true;
+}
 
+bool ping_myself(ros::NodeHandle& nh) {
     ros::ServiceClient client = nh.serviceClient<std_srvs::Empty>("es");
     std_srvs::Empty srv;
-    if (client.call(srv)) {
-        ROS_INFO("OK");
-    } else {
-        ROS_ERROR("ERR");
-        return 1;
+    if (!client.isValid()) {
+        ROS_INFO("The client is not valid.");
+        return false;
     }
+    return client.call(srv);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "test_self_service");
+    ros::NodeHandle nh;
+
+    ros::ServiceServer server = nh.advertiseService("es", &accept_request);
+
+    ros::Rate rate(2);
+    while (ros::ok()) {
+        ros::spinOnce();
+        ROS_INFO("I try...");
+        if (ping_myself(nh)) {
+            ROS_INFO("This worked!");
+        }
+        ROS_INFO("Then I sleep");
+        
+        rate.sleep();
+    }
+
     return 0;
 }
